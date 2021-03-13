9efd03b38bee21c667935500baf4c30453e39c0a
carmit vaknin <carmit.vk@gmail.com>
2021-03-14 01:44:33 +0200

    show reviews

--- fronted/src/cmps/reviewList.vue
+++ fronted/src/cmps/reviewList.vue
@@ -1,19 +1,15 @@
-
-
 <template>
  <ul class="review-list"> 
     <li v-for="review in reviews" :key="review._id" class="review-list-container"> 
         <review-preview :review="review" />
         <div class="actions-container">
-                <button class="btn" @click="remove(review._id)">X</button> 
-                <button class="btn"><router-link :to="'/review/'+review._id">Details</router-link></button>
-                <button class="btn"><router-link :to="'/review/edit/'+review._id">Edit</router-link></button>
+                <!-- <button class="btn" @click="remove(review._id)">X</button>  -->
+                <!-- <button class="btn"><router-link :to="'/review/'+review._id">Details</router-link></button> -->
+                <!-- <button class="btn"><router-link :to="'/review/edit/'+review._id">Edit</router-link></button> -->
         </div>
+        <hr/>
     </li>
 </ul> 
-
-
-
 
 
               
